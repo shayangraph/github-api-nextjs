bf8b411c0b2309db08b8086e969b8766f5a8fb00
Shayan Jafari <shayanja12345@gmail.com>
2023-05-01 16:29:31 +0430

    complete code page and fetch data from github

---
+++ app/code/repos/page.jsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import { FaStar, FaCodeBranch, FaEye } from "react-icons/fa";
+
+async function fetchRepos() {
+  const res = await fetch("https://api.github.com/users/shayangraph/repos");
+  const repos = await res.json();
+  return repos;
+}
+
+const ReposPage = async () => {
+  const repos = await fetchRepos();
+  console.log(repos);
+
+  return (
+    <div className="repos-container">
+      <h2>Repositories</h2>
+      <ul className="repo-list">
+        {repos.map((repo) => (
+          <li key={repo.id}>
+            <Link href={`/code/repos/${repo.name}`}>
+              <h3>{repo.name}</h3>
+              <p>{repo.description}</p>
+              <div className="repo-details">
+                <span>
+                  <FaStar />
+                  {repo.stargazers_count}
+                </span>
+                <span>
+                  <FaCodeBranch />
+                  {repo.forks_count}
+                </span>
+                <span>
+                  <FaEye />
+                  {repo.watchers_count}
+                </span>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ReposPage;
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import { Poppins } from "next/font/google";
+import Header from "./components/Header";
 import "./globals.css";
 
 const poppins = Poppins({
@@ -15,7 +16,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <Header />
+        <main className="container">{children}</main>
+      </body>
     </html>
   );
 }
